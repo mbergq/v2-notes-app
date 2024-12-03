481602f5c039fc9ac178308ecbe991ec98ae122c
Martin <martinberg9@proton.me>
2024-12-03 17:09:24 +0100

    add api proxy, add basic fetch

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from "react";
+
+interface Data {
+  notes: [
+    {
+      id: string;
+      title: string;
+      content: string;
+      background_color_id: number;
+      created_at: string;
+    }
+  ];
+}
+
 function App() {
+  const [data, setData] = useState<null | Data>(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`/api/notes`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const result = await response.json();
+
+      setData(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      {data !== null && data.notes[0].content}
     </>
   );
 }
