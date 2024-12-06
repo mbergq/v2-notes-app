df2246d468eeb3c6e06ac4c48fb478b8974876a7
Martin <martinberg9@proton.me>
2024-12-06 14:16:05 +0100

    add start of form structure

--- frontend/src/components/NoteModal.tsx
+++ frontend/src/components/NoteModal.tsx
@@ -1,5 +1,80 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormInputs = {
+  title: string;
+  content: string;
+  color: string;
+};
+
 function NoteModal() {
-  return <h2>Note modal</h2>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormInputs>();
+  const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
+  return (
+    <div className="min-h-dvh h-full flex justify-center items-center">
+      <div className="w-2/3 h-96 bg-lightpurple">
+        <form
+          method="post"
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col my-4 mx-4"
+        >
+          <button>X</button>
+          <input
+            defaultValue="title"
+            {...register("title")}
+            className="w-1/2"
+          />
+          <input
+            type="radio"
+            {...register("color")}
+            className="self-start"
+            value="green"
+            name="color"
+          />
+          <input
+            type="radio"
+            {...register("color")}
+            className="self-start"
+            value="lightpurple"
+            name="color"
+          />
+          <input
+            type="radio"
+            {...register("color")}
+            className="self-start"
+            value="red"
+            name="color"
+          />
+          <input
+            type="radio"
+            {...register("color")}
+            className="self-start"
+            value="darkpurple"
+            name="color"
+          />
+          <input
+            type="radio"
+            {...register("color")}
+            className="self-start"
+            value="yellow"
+            name="color"
+          />
+          <input
+            type="textarea"
+            defaultValue="content"
+            {...register("content")}
+            className="h-60 mt-2"
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default NoteModal;
