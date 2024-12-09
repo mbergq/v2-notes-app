00a4ac15e9f8c25a5adf1bc8477ec703e4292312
Martin <martinberg9@proton.me>
2024-12-09 10:11:41 +0100

    add styling

--- frontend/src/components/NoteModal.tsx
+++ frontend/src/components/NoteModal.tsx
@@ -17,62 +17,62 @@ function NoteModal() {
 
   return (
     <div className="min-h-dvh h-full flex justify-center items-center">
-      <div className="w-2/3 h-96 bg-lightpurple">
+      <div className="w-2/3 h-full bg-lightpurple">
         <form
           method="post"
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col my-4 mx-4"
         >
-          <button>X</button>
-          <div className="w-fit h-fit">
-            <input
-              defaultValue="title"
-              {...register("title")}
-              className="w-full"
-            />
+          <button className="self-end mr-1">X</button>
+          <input
+            defaultValue="Title.."
+            {...register("title")}
+            className="w-fit"
+          />
+          <div className="w-fit h-fit my-1">
             <input
               type="radio"
               {...register("color")}
-              className="self-start"
               value="green"
               name="color"
+              className="accent-green"
             />
             <input
               type="radio"
               {...register("color")}
-              className="self-start"
               value="lightpurple"
               name="color"
+              className="accent-lightpurple"
             />
             <input
               type="radio"
               {...register("color")}
-              className="self-start"
               value="red"
               name="color"
+              className="accent-red"
             />
             <input
               type="radio"
               {...register("color")}
-              className="self-start"
               value="darkpurple"
               name="color"
+              className="accent-darkpurple"
             />
             <input
               type="radio"
               {...register("color")}
-              className="self-start"
               value="yellow"
               name="color"
+              className="accent-yellow"
             />
           </div>
-          <input
-            type="textarea"
-            defaultValue="content"
+          <textarea
             {...register("content")}
-            className="h-60"
+            className="h-60 mb-5 resize-none"
           />
-          <button type="submit">Submit</button>
+          <button type="submit" className="border w-fit p-2 self-start text-sm">
+            Add note
+          </button>
         </form>
       </div>
     </div>
