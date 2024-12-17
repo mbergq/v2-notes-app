7641f6c02195296c8d8956f6f5c85954d47fc91e
Martin <martinberg9@proton.me>
2024-12-17 11:49:56 +0100

    edit test

--- frontend/cypress/e2e/add-note.cy.ts
+++ frontend/cypress/e2e/add-note.cy.ts
@@ -2,15 +2,12 @@ describe("add a note flow", () => {
   it("successfully adds a new note", () => {
     cy.visit("/");
     cy.contains("new").click();
-    cy.get("#title").type("My very good note title");
+    cy.get("#title").type("shopping list");
+    cy.get("[type='radio']").check("shopping");
     cy.get("[type='radio']").check("red");
-    cy.get("textarea").type(
-      "My very good note content where I've added a lot of good text"
-    );
+    cy.get("textarea").type("apples, bananas, coffee, carrots, yoghurt, bread");
     cy.get("button[type='submit']").click();
-    cy.contains("My very good note title");
-    cy.contains(
-      "My very good note content where I've added a lot of good text"
-    );
+    cy.contains("shopping list");
+    cy.contains("apples, bananas, coffee, carrots, yoghurt, bread");
   });
 });
